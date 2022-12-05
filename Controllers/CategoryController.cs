5b23641f7f9cb0e108d88ab53911312206522ef8
forrest <forrest@forrestbear.org>
2022-12-04 23:44:46 -0500

    troubleshooting category controller

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using MadRiverWeb.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
