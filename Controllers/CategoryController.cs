b3ecdedbf6f30b22b290954b5f4b9b536a8a382d
Forrest Hartley <forrest@forrestbear.org>
2022-12-05 00:33:46 -0500

    troubleshooting Category view

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,9 +1,5 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MadRiverWeb.Models;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace MadRiverWeb.Controllers
 {
     public class CategoryController : Controller
@@ -14,4 +10,3 @@ namespace MadRiverWeb.Controllers
         }
     }
 }
-
